9515affcc8cad8918892eb27687c1e2b5ea89372
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-07 13:19:37 +0200

    creato script per password

---
+++ script.js
@@ -0,0 +1,13 @@
+const userPassWord = alert('Generate your password');
+
+let userFirstName = prompt('First Name');
+userFirstName = (userFirstName);
+
+
+let userLastName = prompt('Last Name');
+userLastName = (userLastName);
+
+let userPreferredColor = prompt('Color');
+userPreferredColor = (userPreferredColor);
+
+console.log(userFirstName + userLastName + userPreferredColor + '24');
